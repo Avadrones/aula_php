6852bb6300dfa41243296220c648eff37513b595
Caetano <seu_avadhontreining@gmail.com>
2024-03-19 22:22:50 -0300

    aula 19/03/2024

---
+++ conexao_mysqli.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost"; // Caminho do banco (IP ou DNS)
+$user = "root"; // Usuário
+$pass = ""; // Senha
+$dbname = "ordem_servico"; // Nome do Banco
+
+$conn = mysqli_connect($host, $user, $pass, $dbname); // Variavel que vai conectar no banco de dados
+
+if ($conn) {
+    echo "Banco de dados conectado com sucesso";
+} else {
+    echo "Falha ao conectar Banco de Dados";
+}
+
+?>
